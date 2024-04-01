8d50582b1a8882e8b7a109f6682e62411225a6dd
Vincent <thisoneis4business@gmail.com>
2024-04-01 18:53:43 +1300

    fix within the same week

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -7,9 +7,6 @@ import { func } from "./func";
  * @returns True if the date is within 7 days of the current date
  */
 export const isWithinTheSameWeek = func((date: Date, now: Date) => {
-  // get different in date and now (in days)
-  const diff = Math.abs(date.getDate() - now.getDate());
-
-  // if the date is within 7 days of the current date
-  return diff <= 7;
+  const diff = (now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24);
+  return diff >= 0 && diff <= 7;
 });
